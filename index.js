9ba8e1a7e8b9d6ed6279c9a009a635c3807ad23f
Herviyana <herviyana0@gmail.com>
2025-08-13 18:01:11 +0700

    perbaikan folder public

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Ganti static folder ke direktori web_app_sibi
-app.use(express.static(path.join(__dirname, "public/web_app_sibi/")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Arahkan root path (/) ke index.html
 // app.get("/", (req, res) => {
